88cfc1a035c9125ee9f7ccb49fe06082b04849be
Mauko Maunde <maukoese@gmail.com>
2017-10-31 13:45:23 +0300

    Done. Yaay!

--- js/ping.js
+++ js/ping.js
@@ -1,19 +1,32 @@
-var lexxy = [];
 var ping = function ( unumber ) {
 	if ( unumber%3 == 0 && unumber%5 !== 0 ) {
-		return "ping!";
+		return "ping";
 	} else if( unumber%3 !== 0 && unumber%5 == 0 ){
 		return "pong";
 	} else if( unumber%3 == 0 && unumber%5 == 0 ){
 		return "pingpong";
-	} else{
+	} else {
 		return unumber;
 	}
 }
 
 var pong = function ( next ) {
-	var arrayLength = lexxy.length;
-	for (var i = 0; i < arrayLength; i++) {
-	    return ping( lexxy[i] );
+	var penty = [];
+	var res = [];
+	for (var i = 1; i <= next; i++) {
+	    penty.push(i)
 	}
+
+	var nexts = penty.map( function ( unumber ) {
+		if ( unumber%3 == 0 && unumber%5 !== 0 ) {
+			return "ping";
+		} else if( unumber%3 !== 0 && unumber%5 == 0 ){
+			return "pong";
+		} else if( unumber%3 == 0 && unumber%5 == 0 ){
+			return "pingpong";
+		} else {
+			return unumber;
+		}
+	} );
+	return nexts;
 }
