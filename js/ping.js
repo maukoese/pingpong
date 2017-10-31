3e86f2930354ba17dbb3c1f80710a6e59e50739c
Mauko Maunde <maukoese@gmail.com>
2017-10-31 12:07:05 +0300

    Added logic.

--- js/ping.js
+++ js/ping.js
@@ -1,5 +1,19 @@
+var lexxy = [];
 var ping = function ( unumber ) {
-	if ( unumber%3 === 0 ) {
+	if ( unumber%3 == 0 && unumber%5 !== 0 ) {
 		return "ping!";
+	} else if( unumber%3 !== 0 && unumber%5 == 0 ){
+		return "pong";
+	} else if( unumber%3 == 0 && unumber%5 == 0 ){
+		return "pingpong";
+	} else{
+		return unumber;
+	}
+}
+
+var pong = function ( next ) {
+	var arrayLength = lexxy.length;
+	for (var i = 0; i < arrayLength; i++) {
+	    return ping( lexxy[i] );
 	}
 }
